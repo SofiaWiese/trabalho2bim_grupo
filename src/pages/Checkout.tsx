4f92f04ad4ff6ae6db1013c837840d91379213d2
Sofia Wiese <sofiawiese2010@gmail.com>
2026-04-15 13:57:47 -0300

    só o começo

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,4 +1,8 @@
 const Checkout =({...props})=>{
-    return<div {...props}>Checkout</div>
+    return<div {...props}>
+        <h2>Checkout</h2>
+        <h3>Resumo do pedido</h3>
+        <input type="text" placeholder="Digite aqui suas informações"></input>
+    </div>
 }
 export default Checkout
