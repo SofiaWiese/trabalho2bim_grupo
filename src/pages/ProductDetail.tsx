0d7c6703afe9f9001c14e7eb09d01e4e5921c340
Arthur <arthur.lionaguiar@email.com>
2026-04-15 13:59:10 -0300

    msg

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,4 +1,48 @@
 const ProductDetail =({...props})=>{
-    return<div {...props}>ProductDetail</div>
+    return<div> 
+
+
+<h1> Sapoato </h1>
+  
+<img 
+    className="w-150"
+    src="https://flavios.vtexassets.com/arquivos/ids/226744-1600-1600?v=638312696032030000&width=1600&height=1600&aspect=true"
+    alt="img tenis"
+    />
+
+<img 
+    className="w-35"
+    src="https://flavios.vtexassets.com/arquivos/ids/226745-1600-1600?v=638312696152270000&width=1600&height=1600&aspect=true"
+    alt="img tenis"
+     />
+    
+<img
+    className="w-35"
+    src="https://flavios.vtexassets.com/arquivos/ids/226746-1600-1600?v=638312696233870000&width=1600&height=1600&aspect=true"
+    alt="img tenis"
+    />
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
 }
 export default ProductDetail
+
