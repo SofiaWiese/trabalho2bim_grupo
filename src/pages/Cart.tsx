552499e3955a8a14cc9b0489a5d6247995d007cf
ryanbento <bentopreciliano@gmail.com>
2026-05-05 16:39:32 -0300

    Enhance Cart component with navigation and layout
    Added navigation functionality and improved layout.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,36 @@
-const Cart =({...props})=>{
-    return<div {...props}>Cart</div>
+import { useAppNavigation } from "../hooks/useNavigation"
+
+const Cart = ({ ...props }) => {
+  const {goBack} = useAppNavigation()  
+  const {goToHome} = useAppNavigation()
+
+
+  return <div {...props} >
+    <div className="flex items-center flex-col w-full h-[250px] ">
+     <h1>Sapoato</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjh0nc-PXNOFUmdVVUIrHTJlmiWyMxF9da5g&s" />
+    </div>
+      <h3>
+        O carrinho está <span style={{ "fontWeight": "bold" }}>vazio</span>
+      </h3>
+
+    <div>
+      <div onClick={goBack} className="cursor-pointer"><h2>Voltar</h2></div>
+      </div>
+
+
+
+   <div className="cursor-pointer"> <footer>Finalizar compra</footer> </div>
+
+   <div className=" wd-widget">
+
+
+
+   </div>
+
+  
+
+  </div>
 }
 export default Cart
+
